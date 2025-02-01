43f2d9375f92bd468716bc322c25e977b4b312dc
zdenda142 <77118689+zdenda142@users.noreply.github.com>
2025-02-01 16:36:12 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'ZdenDako'
+description 'Custom Ped Script using ox_lib'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+dependencies {
+    'ox_lib'
+}
